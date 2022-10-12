eeeef476b3e0e633501368d64d3b3d0069ef5fbe
nso89 <114881729+nso89@users.noreply.github.com>
2022-10-12 12:11:11 -0400

    Update using-set.py

--- using-set/using-set.py
+++ using-set/using-set.py
@@ -6,18 +6,25 @@ def main():
     empty_set = set()
 
     # Creating a set with elements:
-    numbers = set([21,57,17,41,42,60,83,69,87])
-    print(numbers) # Output: {69, 41, 42, 17, 83, 21, 87, 57, 60}
+    numbers = set([1,2,3,4,5])
+    print(numbers) # Output: {1, 2, 3, 4, 5}
+
+    # Using add():
+    numbers.add(69)
+    print(numbers) # Output: {1, 2, 3, 4, 5, 69}
+
+    # Using update():
+    numbers.update([1,3,5,7,8,9,10])
+    print(numbers) # Output: {1, 2, 3, 4, 5, 69, 7, 8, 9, 10}
 
     # Using intersection():
-    other_numbers = set([70,34,69,44,25,17])
-    print(numbers.intersection(other_numbers)) # Output: {17, 69}
-
+    print(numbers.intersection([1,3,5,7,8,9,10])) # Output: {1, 3, 5, 7, 8, 9, 10}
+   
     # Using union():
-    print(numbers.union(other_numbers)) # Output: {34, 69, 70, 41, 42, 44, 17, 83, 21, 87, 57, 60, 25}
+    print(numbers.union([1,3,5,7,8,9,10])) # Output: {1, 2, 3, 4, 5, 69, 7, 8, 9, 10}
 
     # Using setdifference():
-    print(numbers.difference(other_numbers)) # Output: {41, 42, 83, 21, 87, 57, 60}
+    print(numbers.difference([1,3,5,7,8,9,10])) # Output: {2, 4, 69}
 
 if __name__ == "__main__":
     main()
