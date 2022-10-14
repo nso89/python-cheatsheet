d0d5fcad3654e269c75917a1b321198fcd24f9df
nso89 <114881729+nso89@users.noreply.github.com>
2022-10-14 15:52:26 -0400

    Create using-tuple.py

---
+++ using-sequence/using-tuple/using-tuple.py
@@ -0,0 +1,15 @@
+def main():
+
+    # Creating an empty tuple:
+    empty_tuple : tuple = ()
+
+    # Creating a tuple with elements
+    svetlana  : tuple = ("Empress Svetlana",25,"Trance")
+    print(svetlana) # Output: ('Empress Svetlana', 25, 'Trance')
+
+    # Unpacking a tuple:
+    first_element, *middle, last_element = svetlana
+    print(f"{first_element}, {middle}, {last_element}") # Output: Empress Svetlana, [25], Trance
+
+if __name__ == "__main__":
+    main()
