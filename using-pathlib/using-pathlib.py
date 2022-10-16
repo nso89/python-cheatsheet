97ce5d7c8ff256d99fca9b32d564086ba7f76ad8
nso89 <114881729+nso89@users.noreply.github.com>
2022-10-16 10:47:30 -0400

    Update using-pathlib.py

--- using-pathlib/using-pathlib.py
+++ using-pathlib/using-pathlib.py
@@ -3,14 +3,14 @@ from pathlib import Path
 def main():
 
     # Create a Path Object:
-    saved_games = Path("Saved 3D Games")
+    saved_3D_games = Path("Saved 3D Games")
 
     # Using home():
     print(Path.home()) # Output: C:\Users\nso89
 
     # Using joinpath():
     user_profile = Path.home()
-    print(user_profile.joinpath(saved_games)) # Output: C:\Users\nso89\Saved 3D Games
+    print(user_profile.joinpath(saved_3D_games)) # Output: C:\Users\nso89\Saved 3D Games
 
     # Using mkdirs():
     complete_path_to_saved_3d_games = user_profile.joinpath(saved_games)
