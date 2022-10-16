30fa66baedbb6965ece62fbef8773c0e1dcde233
nso89 <114881729+nso89@users.noreply.github.com>
2022-10-16 10:45:57 -0400

    Update using-pathlib.py

--- using-pathlib/using-pathlib.py
+++ using-pathlib/using-pathlib.py
@@ -3,18 +3,21 @@ from pathlib import Path
 def main():
 
     # Create a Path Object:
-    saved_games = Path("Saved Games")
+    saved_games = Path("Saved 3D Games")
 
     # Using home():
     print(Path.home()) # Output: C:\Users\nso89
 
     # Using joinpath():
     user_profile = Path.home()
-    print(user_profile.joinpath(saved_games)) # Output: C:\Users\nso89\Saved Games
+    print(user_profile.joinpath(saved_games)) # Output: C:\Users\nso89\Saved 3D Games
 
     # Using mkdirs():
-    complete_path_to_saved_games = user_profile.joinpath(saved_games)
-    complete_path_to_saved_games.mkdir(parents=True,exist_ok=True)
+    complete_path_to_saved_3d_games = user_profile.joinpath(saved_games)
+    complete_path_to_saved_3d_games.mkdir(parents=True,exist_ok=True)
+
+    # Using rmdir():
+    complete_path_to_saved_3d_games.rmdir()
 
 if __name__ == "__main__":
     main()
