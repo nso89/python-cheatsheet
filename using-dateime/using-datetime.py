ea541a261b6b97de552333b9b3726c5bb6cc402a
nso89 <114881729+nso89@users.noreply.github.com>
2022-11-01 11:00:35 -0400

    Create using-datetime.py

---
+++ using-dateime/using-datetime.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+import time
+
+def main():
+
+    # Using datetime.now() with strftime():
+    print(datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+    # Using time.strftime():
+    print(time.strftime("%Y%m%d-%H%M%S"))
+
+if __name__ == "__main__":
+    main()
