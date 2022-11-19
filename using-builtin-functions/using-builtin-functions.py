ffeeeee0f3073e687344aa34c11930a26e2a3207
nso89 <114881729+nso89@users.noreply.github.com>
2022-11-19 14:20:19 -0500

    Update using-builtin-functions.py

--- using-builtin-functions/using-builtin-functions.py
+++ using-builtin-functions/using-builtin-functions.py
@@ -10,7 +10,7 @@ def main():
     
     # Using filder():
     zero_reminder : List[int] = [1,2,3,4,5,6,7,8,9,10]
-    for number in filter(divide_by_two,zero_reminder):
+    for number in filter(divide_by_two, zero_reminder):
         print(number)
         
     # Output:
